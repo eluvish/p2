b859154f20ce63ffd4bc70a56e1c3cf6e5580b76
Eli Luvish <iluvish@gmail.com>
2015-09-20 15:35:41 -0400

    Updated logic.php for better readability

--- logic.php
+++ logic.php
@@ -1,46 +1,49 @@
 <?php
 
-// Cast the number of words the user wants to an int
+// All of the code is encased in this if statement so that no errors show up when the user initially visits the site.
 if (!empty($_GET))
 {
+
+  // Cast the number of words the user wants to an int
   $num = (int) htmlspecialchars($_GET["number_of_words"]);
 
-// Array to hold file contents of word.txt
-$f_contents = file("words.txt");
+  // Array to hold file contents of word.txt
+  $f_contents = file("words.txt");
 
-// Count the number of lines in the file - later to be used in the rand() function
-$lines = count($f_contents) - 1;
+  // Count the number of lines in the file - later to be used in the rand() function
+  $lines = count($f_contents) - 1;
 
-// Declare the output array
-$output = array();
+  // Declare the output array
+  $output = array();
 
-// Push $num elements in to the output array and include a dash in between
-for ($i = 0; $i < $num; $i++)
-{
-		array_push($output, $f_contents[rand(0, $lines)]);
+  // Push $num elements in to the output array and include a dash in between
+  for ($i = 0; $i < $num; $i++)
+  {
+		  array_push($output, $f_contents[rand(0, $lines)]);
 
-    // Add a hyphen between words unless we have reached the final word
-    if ($i !== $num-1){
+      // Add a hyphen between words unless we have reached the final word
+      if ($i !== $num-1)
+      {
 				array_push($output, "-");
-		}
+		  }
+  }
 
-}
+  // if the user requested a number, add it to the array
+  if (isset($_GET["add_number"]))
+  {
+	   array_push($output, rand(0,9));
+  }
 
-// if the user requested a number, add it to the array
-if (isset($_GET["add_number"]))
-{
-	array_push($output, rand(0,9));
-}
+  // if the user requested a symbol, add it to the array
+  if (isset($_GET["add_symbol"]))
+  {
+	   $symbols = array("!","@", "#","$","%","^","&","*","(",")");
+	    array_push($output, $symbols[array_rand($symbols)]);
+  }
 
-// if the user requested a symbol, add it to the array
-if (isset($_GET["add_symbol"]))
-{
-	$symbols = array("!","@", "#","$","%","^","&","*","(",")");
-	array_push($output, $symbols[array_rand($symbols)]);
-}
-
-// Trim the white space
-// Source: http://stackoverflow.com/questions/5762439/how-to-trim-white-spaces-of-array-values-in-php
-$trimmed = array_map('trim', $output);
+  // Trim the white space
+  // Source: http://stackoverflow.com/questions/5762439/how-to-trim-white-spaces-of-array-values-in-php
+  $trimmed = array_map('trim', $output);
+  
 }
  ?>
